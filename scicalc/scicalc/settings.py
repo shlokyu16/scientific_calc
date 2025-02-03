40b74d910fb69227dca9a093d72dbe66d8e7ea7b
shlok <103650295+shlokyu16@users.noreply.github.com>
2025-02-02 23:33:24 -0800

    Update settings.py

--- scicalc/scicalc/settings.py
+++ scicalc/scicalc/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = 'django-insecure-$b003e&)vbfvgs&j&b_d^4xiq$c9)%fu)@xx8#0fj56oirjzk@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "sk-scientific-calculator.onrender.com",
-    "127.0.0.1", 
-    "localhost",
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
