e6a9a48a110115259ed5c061e0db19b7ab038d73
shlok <103650295+shlokyu16@users.noreply.github.com>
2025-12-21 02:09:11 -0800

    Update settings.py

--- scicalc/settings.py
+++ scicalc/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$b003e&)vbfvgs&j&b_d^4xiq$c9)%fu)@xx8#0fj56oirjzk@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1],definite-dinah-shlokyu16-7f98957e.koyeb.app/").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
 
 
 # Application definition
